cab0cb364c6af2168311c96c271639649b2558ea
Bahul Jain <bkj2111@columbia.edu>
2016-11-07 16:24:32 -0500

    removed .h and .cc for Vector classs

--- include/Vector.h
+++ include/Vector.h
@@ -5,29 +5,60 @@
 #ifndef RAYTRA_VECTOR_H
 #define RAYTRA_VECTOR_H
 
+#include <assert.h>
+#include <iostream>
+#include <math.h>
+
 class Vector {
 public:
     float i, j, k;
 
-    Vector();
+    Vector() {
+        this->i = this->j = this->k = 0;
+    };
 
-    Vector(float, float, float);
+    Vector(float i, float j, float k) {
+        this->i = i;
+        this->j = j;
+        this->k = k;
+    };
 
-    float mag() const;
+    float mag() const {
+        return sqrtf(powf(i, 2.0) + powf(j, 2.0) + powf(k, 2.0));
+    };
 
-    Vector norm() const;
+    Vector norm() const {
+        float mag = this->mag();
+        assert (mag != 0.0);
 
-    Vector times(float) const;
+        return Vector(i / mag, j / mag, k / mag);
+    };
 
-    Vector cross(const Vector &) const;
+    Vector times(float c) const {
+        return Vector(i * c, j * c, k * c);
+    };
 
-    float dot(const Vector &) const;
+    Vector cross(const Vector &vec) const {
+        return Vector(j * vec.k - k * vec.j,
+                      k * vec.i - i * vec.k,
+                      i * vec.j - j * vec.i);
+    };
 
-    Vector plus(const Vector &) const;
+    float dot(const Vector &vec) const {
+        return i * vec.i + j * vec.j + k * vec.k;
+    };
 
-    Vector operator-() const;
+    Vector plus(const Vector &vec) const {
+        return Vector(i + vec.i, j + vec.j, k + vec.k);
+    };
 
-    void printVector() const;
+    Vector operator-() const {
+        return Vector(-i, -j, -k);
+    };
+
+    void printVector() const {
+        std::cout << i << "i + " << j << "j + " << k << "k" << std::endl;
+    };
 };
 
 #endif //RAYTRA_VECTOR_H
